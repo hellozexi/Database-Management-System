4e2637ffe2a2a6a3e9f9b865596c853d13be4548
YangWu <wuyang@wustl.edu>
2019-02-02 23:15:19 -0600

    forget table

---
+++ src/hw1/Table.java
@@ -0,0 +1,17 @@
+package hw1;
+
+public class Table extends Catalog {
+	
+	String tableName;
+	String primaryKey;
+	int tableId;
+	
+	//constructor
+	
+	public Table(String tableName, String primaryKey) {
+		this.tableName = tableName;
+		this.primaryKey = primaryKey;
+		this.tableId = tableId;
+	}
+
+}
